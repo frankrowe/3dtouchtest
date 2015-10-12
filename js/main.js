01cb8d99ed2c9491e33b57cd54a8778e7ecba7be
Frank Rowe <fsrowe@gmail.com>
2015-10-12 12:49:41 -0400

    add touch events

--- js/main.js
+++ js/main.js
@@ -3,7 +3,51 @@ $(document).ready(function(){
   var map = new L.Map('map').setView(new L.LatLng(38.85, -77), 7);
 
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    detectRetina: true
   }).addTo(map);
+
+  var element = document.getElementById('map');
+  var touch = null;
+
+  addForceTouchToElement(element);
+
+  function onTouchStart(e) {
+    e.preventDefault();
+    checkForce(e);
+  }
+
+  function onTouchMove(e) {
+    e.preventDefault();
+    checkForce(e);
+  }
+
+  function onTouchEnd(e) {
+    e.preventDefault();
+    touch = null;
+  }
+
+  function checkForce(e) {
+    touch = e.touches[0];
+    setTimeout(refreshForceValue.bind(touch), 10);
+  }
+
+  function refreshForceValue() {
+    var touchEvent = this;
+    var forceValue = 0;
+    if(touchEvent) {
+      forceValue = touchEvent.force || 0;
+      setTimeout(refreshForceValue.bind(touch), 10);
+    }else{
+      forceValue = 0;
+    }
+    console.log(forceValue);
+  }
+
+  function addForceTouchToElement(elem) {
+    elem.addEventListener('touchstart', onTouchStart, false);
+    elem.addEventListener('touchmove', onTouchMove, false);
+    elem.addEventListener('touchend', onTouchEnd, false);
+  }
 
 });
